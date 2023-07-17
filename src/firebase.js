f4dbf030b19ea4fed6271fda19d7c70b9a0ff7c3
Arnab Majumder <arnabmajumdar399@gmail.com>
2023-07-17 21:35:33 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBISsLULkCi7d6EUSDosiHuMVFWUWSkjKQ",
+  authDomain: "chat-app-c7608.firebaseapp.com",
+  projectId: "chat-app-c7608",
+  storageBucket: "chat-app-c7608.appspot.com",
+  messagingSenderId: "121853071489",
+  appId: "1:121853071489:web:424082363cbae0bfc1fd80"
+};
+
+
+export const app = initializeApp(firebaseConfig);
